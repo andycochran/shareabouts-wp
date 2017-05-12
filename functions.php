1773a3409cf173ca6249f7a90c321b76ebf21a93
Andy Cochran <acochran@council.nyc.gov>
2017-05-12 17:00:58 -0400

    hide admin bar

--- functions.php
+++ functions.php
@@ -5,3 +5,6 @@ require_once(get_template_directory().'/assets/functions/comments.php');
 require_once(get_template_directory().'/assets/functions/enqueue-scripts.php');
 require_once(get_template_directory().'/assets/functions/menus.php');
 require_once(get_template_directory().'/assets/functions/places.php');
+
+// Turn off the Admin Bar until it works with AJAX page loading
+add_filter('show_admin_bar', '__return_false');
